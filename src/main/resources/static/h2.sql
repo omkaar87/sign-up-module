37bb8d3df07d793e3f681021632cec2f6049dbf7
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2019-07-09 07:11:27 +0000

    made some changes in h2 and properties

--- src/main/resources/static/h2.sql
+++ src/main/resources/static/h2.sql
@@ -3,20 +3,19 @@
 CREATE TABLE `authorities` (
   `username` varchar(50) NOT NULL,
   `authority` varchar(50) NOT NULL
-)
+);
 
 CREATE TABLE `reset` (
   `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `resetToken` varchar(50) DEFAULT NULL
-) 
+);
 
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` char(68) NOT NULL,
   `enabled` tinyint(1) NOT NULL
-)
-
+);
 --Adding keys
 
 ALTER TABLE `authorities`
@@ -29,7 +28,7 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
 
 ALTER TABLE `reset`
-  ADD PRIMARY KEY (`id`)
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `reset`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
