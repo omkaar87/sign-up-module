d1571e63aa3b4dc9e3b333a46a69aa7a89960cfa
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2019-07-16 07:31:37 +0000

    added mapping for admin

--- src/main/java/com/mcq/controller/LoginController.java
+++ src/main/java/com/mcq/controller/LoginController.java
@@ -6,27 +6,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-	
+
 	@GetMapping("/loginPage")
 	public String showMyLoginPage()
 	{
 		return "login-page";
 	}
-	
+
 	//add  request mapping for leaders
 	@GetMapping("/leaders")
 	public String showLeaders() {
-		
+
 		return "leaders";
 	}
-	
+
+    //add  request mapping for admin
+	@GetMapping("/admin")
+	public String showAdminPage() {
+
+		return "admin";
+	}
+
 	//add  request mapping for systems
 	@GetMapping("/systems")
 	public String showSystems() {
-		
+
 		return "systems";
 	}
-	
+
 	//access denied
 	@GetMapping("/access-denied")
 	public String access()
